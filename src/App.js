8eba7220a7948309232d693ef9d337b76b3e15c7
novelus1 <krzysztof.truszkowski@outlook.com>
2023-07-22 01:36:00 +0200

    Add proper positioning and sizes, change image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
-import page from './page.png';
+import page from "./page.jpg";
 
 function App() {
   return (
     <div className="hero-section-container">
-      <div className="header-logo">Header Logo</div>
-      <div className="header-link-text">
-        <div>header link one</div>
-        <div>header link one</div>
-        <div>header link one</div>
+      <div className="header-link-text-container">
+        <div className="header-logo">Header Logo</div>
+        <div className="header-links">
+          <div>header link one</div>
+          <div>header link two</div>
+          <div>header link three</div>
+        </div>
       </div>
       <div className="hero-main-text-container">
-        <div>This website is awesome</div>
-        <p>
-          This website has some subtext that goes here under the main title.
-          It's a smaller font and the color is lower contrast
-        </p>
+        <div className="hero-main-text-wrapper">
+          <span>This website is awesome</span>
+          <p>
+            This website has some subtext that goes here under the main title.
+            It's a smaller font and the color is lower contrast
+          </p>
+          <button className="sign-up-button">Sign up</button>
+        </div>
+        <img src={page} alt="Landing page" id="landing-page" />
       </div>
-      <button className="sign-up-button">Sign up</button>
-      <img src={page} alt="Landing page" id="landing-page"/>
     </div>
   );
 }
