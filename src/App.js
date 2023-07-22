6639b1c1d75ac28d4998d3fca8f58012d2b40af8
novelus1 <krzysztof.truszkowski@outlook.com>
2023-07-22 13:11:41 +0200

    Add random information section, body font change

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import "./App.css";
 import page from "./page.jpg";
+import frog from "./frog.png";
 
 function App() {
   return (
-    <div className="hero-section-container">
-      <div className="header-link-text-container">
-        <div className="header-logo">Header Logo</div>
-        <div className="header-links">
-          <div>header link one</div>
-          <div>header link two</div>
-          <div>header link three</div>
+    <div>
+      <div className="hero-section-container">
+        <div className="header-links-container">
+          <div className="header-logo">Header Logo</div>
+          <div className="header-links">
+            <div>header link one</div>
+            <div>header link two</div>
+            <div>header link three</div>
+          </div>
+        </div>
+        <div className="hero-main-text-container">
+          <div className="hero-main-text-wrapper">
+            <p className="hero-bold-text">This website is awesome</p>
+            <p>
+              This website has some subtext that goes here under the main title.
+              It's a smaller font and the color is lower contrast
+            </p>
+            <button className="sign-up-button">Sign up</button>
+          </div>
+          <img src={page} alt="Landing page" id="landing-page" />
         </div>
       </div>
-      <div className="hero-main-text-container">
-        <div className="hero-main-text-wrapper">
-          <span>This website is awesome</span>
-          <p>
-            This website has some subtext that goes here under the main title.
-            It's a smaller font and the color is lower contrast
-          </p>
-          <button className="sign-up-button">Sign up</button>
+      <div className="random-information">Some random information.</div>
+      <div className="image-section-container">
+        <div className="image-section">
+          <div className="image-and-description">
+            <div
+              className="image"
+              style={{ backgroundImage: `url(${frog})` }}
+            ></div>
+            <div className="description">Pepe</div>
+          </div>
+          <div className="image-and-description">
+            <div className="image"></div>
+            <div className="description">Description 1</div>
+          </div>
+          <div className="image-and-description">
+            <div className="image"></div>
+            <div className="description">Description 2</div>
+          </div>
+          <div className="image-and-description">
+            <div className="image"></div>
+            <div className="description">Description 3</div>
+          </div>
         </div>
-        <img src={page} alt="Landing page" id="landing-page" />
       </div>
     </div>
   );
